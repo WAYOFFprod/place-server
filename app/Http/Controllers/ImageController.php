18767d6c96a4b2881925dc68d21c1989ad5aafbc
WAYOFFprod <raphael@wayoff.ch>
2022-05-23 11:41:45 +0200

    improved image validation

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -10,30 +10,42 @@ use Validator;
 use App\Models\Image;
 use App\Models\Canvas;
 
+use App\Rules\Base64;
+
 class ImageController extends Controller
 {
     public function upload(Request $request) {
         $file = null;
+        $path = '';
         $canvas_id = $request->input('canvas_id');
 
         $validator = Validator::make($request->all(),[ 
-            'file' => 'required|mimes:jpg,png,jpeg,gif|max:2048',
-        ]);   
+            'width' => 'required|numeric',
+            'height' => 'required|numeric',
+            'canvas_id' => 'required|numeric',
+            'name' => 'required|string|max:255'
+        ]);
+
+        $validator->sometimes('file', new Base64, function ($input) {
+            return str_contains($input->file, ';base64,');
+        });
+        $validator->sometimes('file', 'required|mimes:jpg,png,jpeg,gif|max:10000', function ($input) {
+            return !str_contains($input->file, ';base64,');
+        });
 
         if($validator->fails()) {         
             // check if base64 image 
-            if(str_contains($request->input('file'), ';base64,')) {
-                $bin = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '',$request->input('file'))); 
-                $file = imageCreateFromString($bin);
-                Storage::disk('public')->put('/' . 'canvas-'.$canvas_id.'.png', $bin);
-                $path = 'storage/' . 'canvas-'.$canvas_id.'.png';
-            } else {
-                return response()->json(['error'=>$validator->errors()], 401);
-            }
+            return response()->json(['error'=>$validator->errors()], 401);
+        } else {
+            print_r("not failed");
         }
 
-
-        if($file == null && $request->file('file')) {
+        if(str_contains($request->input('file'), ';base64,')) {
+            $bin = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '',$request->input('file'))); 
+            $file = imageCreateFromString($bin);
+            Storage::disk('public')->put('/' . 'canvas-'.$canvas_id.'.png', $bin);
+            $path = 'storage/' . 'canvas-'.$canvas_id.'.png';
+        } else if($file == null && $request->file('file')) {
             $file = $request->file('file');
             $path = $file->storeAs('public', 'canvas-'.$canvas_id.'.png' );
             $path = str_replace('public/', 'storage/', $path);
@@ -46,7 +58,7 @@ class ImageController extends Controller
             'name' => $request->input('name'),
             'path' => $path,
         ]);
-        print_r($image->id);
+
         $canvas = Canvas::findOrFail($canvas_id);
         $canvas->preview()->associate($image);
 
